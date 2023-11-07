ca0a2cd3ed577d60aebba652f8e30ce2a8054796
Tim Lynch <tlynch@hbs.net>
2023-11-07 14:08:56 -0600

    Saved ZipCode as cookie for preloading

---
+++ MantaCloud_Weather/Interfaces/ICookieStorageAccessor.cs
@@ -0,0 +1,9 @@
+﻿namespace MantaRays_Weather.Interfaces
+{
+    public interface ICookieStorageAccessor
+    {
+        ValueTask DisposeAsync();
+        Task<T> GetValueAsync<T>(string key);
+        Task SetValueAsync<T>(string key, T value);
+    }
+}
---
+++ MantaCloud_Weather/wwwroot/js/CookieStorage.js
@@ -0,0 +1,19 @@
+﻿export function get(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+
+export function set(key, value) {
+    var date = new Date();
+    date.setTime(date.getTime() + (5 * 24 * 60 * 60 * 1000));
+    var expires = "; expires=" + date.toGMTString();
+    document.cookie = `${key}=${value}${expires}`;
+}
+
